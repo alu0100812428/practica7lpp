32dc0e29eb47f1bb77534cb96a7fbe460fa47115
Pablo Sebastián Caballero <alu0100812428@ull.edu.es>
2016-11-02 23:04:17 +0000

    Pruebas modificadas

--- dieta/dieta.gemspec
+++ dieta/dieta.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["alu0100812428"]
   spec.email         = ["alu0100812428@ull.edu.es"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Dieta}
+  spec.description   = %q{clase para representar menús de una dieta.}
+  spec.homepage      = "https://github.com/ULL-ESIT-LPP-1617/tdd-menu-lpp-40"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
   # to allow pushing to a single host or delete this section to allow pushing to any host.
--- dieta/spec/dieta_spec.rb
+++ dieta/spec/dieta_spec.rb
@@ -1,29 +1,17 @@
 require "spec_helper"
-require "lib/dieta"
+require "dieta/dieta"
 
-describe Dieta do
-  #it "has a version number" do
-   # expect(Dieta::VERSION).not_to be nil
-  #end
-
-  #it "does something useful" do
-   # expect(false).to eq(true)
-  #end
-  before :each do
-  end
-  
+describe Menu do
+ 
+ before :each do
+  @dieta=Menu.new("ALMUERZO")
+ end
+ 
   it "Debe existir la clase Dieta" do
-    expect(Dieta.new().is_a?(Dieta)).to eq(true)
+    expect(@dieta.is_a?(Menu)).to eq(true)
   end
   
   it "Debe existir un título para el menú" do
-    expect(Dieta.new("")).to raise_error("Titulo no válido")
-    expect(Dieta.new("papa")).to raise_error("Titulo no válido")
-  end
-  
-  
-  it "Debe existir el porcetaje que el menú representa de la ingesta diaria" do
-    expect(Dieta.new("ALMUERZO",[30,35,40])).to raise_error("Porcentaje no válido. Intervalo incorrecto.")
-    expect(Dieta.new("ALMUERZO",[])).to raise_error("Porcentaje no válido. No se ha especificado.")
+    expect(@dieta.titulo).to eq("ALMUERZO")
   end
 end
