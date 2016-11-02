15c9347148df43aff8052bb9e6331fd5ecbb946d
Oscar Catari Gutierrez <alu0100825893@ull.edu.es>
2016-11-02 23:20:47 +0000

    Implementacion del titulo

--- dieta/lib/dieta/dieta.rb
+++ dieta/lib/dieta/dieta.rb
@@ -1,3 +1,16 @@
 class Menu
-    
+    attr_reader :titulo
+    def initialize(titulo)
+        regexp = /\s*(\w*)\s*(DESAYUNO|MEDIO MAÑANA|ALMUERZO|MERIENDA|CENA)\s*(\w*)\s*/
+        resultado = regexp.match(titulo)
+        
+        if (resultado == nil )
+            raise "Titulo no válido"
+        elsif ((resultado[1] != "") || (resultado[3] != ""))
+            raise "Titulo no válido"
+        end
+        
+        @titulo = resultado[2]
+        
+    end
 end
