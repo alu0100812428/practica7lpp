554f9fd27f67d5db56f69d2235b85e05ec73b979
Pablo Sebastián Caballero <alu0100812428@ull.edu.es>
2016-11-02 14:17:49 +0000

    Pruebas de clase, titulo y porcentaje

--- dieta/spec/dieta_spec.rb
+++ dieta/spec/dieta_spec.rb
@@ -1,11 +1,29 @@
 require "spec_helper"
+require "lib/dieta"
 
 describe Dieta do
-  it "has a version number" do
-    expect(Dieta::VERSION).not_to be nil
-  end
+  #it "has a version number" do
+   # expect(Dieta::VERSION).not_to be nil
+  #end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  #it "does something useful" do
+   # expect(false).to eq(true)
+  #end
+  before :each do
+  end
+  
+  it "Debe existir la clase Dieta" do
+    expect(Dieta.new().is_a?(Dieta)).to eq(true)
+  end
+  
+  it "Debe existir un título para el menú" do
+    expect(Dieta.new("")).to raise_error("Titulo no válido")
+    expect(Dieta.new("papa")).to raise_error("Titulo no válido")
+  end
+  
+  
+  it "Debe existir el porcetaje que el menú representa de la ingesta diaria" do
+    expect(Dieta.new("ALMUERZO",[30,35,40])).to raise_error("Porcentaje no válido. Intervalo incorrecto.")
+    expect(Dieta.new("ALMUERZO",[])).to raise_error("Porcentaje no válido. No se ha especificado.")
   end
 end
