43272238360542f9eb1c0ff1abf60ccd649884e8
Oscar Catari Gutierrez <alu0100825893@ull.edu.es>
2016-11-03 12:04:23 +0000

    Pruebas de metodo para obtener VCT,proteinas,grasas,hidratos,menu formateado

--- dieta/spec/dieta_spec.rb
+++ dieta/spec/dieta_spec.rb
@@ -87,6 +87,25 @@ describe Menu do
     expect(@dieta.get_conjunto_platos).to eq("- Macarrones con salsa de tomate y queso parmesano, 1 1/2 cucharón, 200 g\n- Escalope de ternera, 1 bistec mediano, 100 g\n- Ensalada básica con zanahoria rallada, guarnición, 120 g\n- Mandarina, 1 grande, 180 g\n- Pan de trigo integral, 1 rodaja, 20 g")
   end
   
+  it "Existe un metodo para obtener el V.C.T." do
+    expect(@dieta.get_vct).to eq("785,9 kcal")
+  end
+  
+  it "Existe un metodo para obtener el porcentaje de proteinas" do
+    expect(@dieta.get_proteinas).to eq("19%")
+  end
+  
+  it "Existe un metodo para obtener el porcentaje de grasas" do
+    expect(@dieta.get_proteinas).to eq("34%")
+  end
+  
+  it "Existe un metodo para obtener el porcentaje de hidratos de carbono" do
+    expect(@dieta.get_proteinas).to eq("47%")
+  end
+  
+  it "Existe un metodo para obtener el menu formateado" do
+    expect(@dieta.to_s).to eq("ALMUERZO (30 - 35%)\n- Macarrones con salsa de tomate y queso parmesano, 1 1/2 cucharón, 200 g\n- Escalope de ternera, 1 bistec mediano, 100 g\n- Ensalada básica con zanahoria rallada, guarnición, 120 g\n- Mandarina, 1 grande, 180 g\n- Pan de trigo integral, 1 rodaja, 20 g\nV.C.T. | % 785,9 kcal | 19% - 34% - 47%")
+  end
   
   
   
